26012ba58bb69546eec6f255e324aa90e21b8121
Kabbo | Frontend Developer <160781484+siyam1100@users.noreply.github.com>
2026-02-24 20:08:47 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // Public Electrum Server (SSL)
+    ELECTRUM_HOST: "electrum.blockstream.info",
+    ELECTRUM_PORT: 50002,
+    
+    // Local DB settings
+    DB_NAME: "ordinals.db",
+    
+    // The block height to start indexing from (Ordinals started at 767430)
+    START_BLOCK: 767430
+};
